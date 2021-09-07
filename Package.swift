f958948a76aa9e5bbe31566adcd76cb2e59e042c
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2021-09-07 14:51:12 +0700

    Update path

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "QKMRZParser",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "QKMRZParserTests",
-            dependencies: ["QKMRZParser"]),
+            dependencies: ["QKMRZParser"],
+            path: "Tests"),
     ]
 )
